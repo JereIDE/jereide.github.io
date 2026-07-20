aaeb3447af50497d9264c424a14a7b4b9acf55a9
Jeremy-Qian <jeremytheking2013@gmail.com>
2026-07-20 20:51:00 +0800

    Add a floating navbar, a releases page, a static navbar, placeholder content, and more

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const downloadBtn = document.querySelector('.button-accent');
+const downloadBtn = document.querySelector(".button-accent");
+const navbar = document.querySelector(".navbar");
+const container = document.querySelector(".container");
 
-fetch('https://api.github.com/repos/JereIDE/JereIDE/releases/latest')
-    .then(res => res.json())
-    .then(data => {
-        const tag = data.tag_name;
-        downloadBtn.textContent = `Download Version ${tag.replace(/^v/i, '')}`;
-        downloadBtn.href = data.html_url;
-    })
-    .catch(() => {
-        downloadBtn.textContent = 'Download Latest Release';
-    });
+fetch("https://api.github.com/repos/JereIDE/JereIDE/releases/latest")
+  .then((res) => res.json())
+  .then((data) => {
+    const tag = data.tag_name;
+    downloadBtn.textContent = `Download Version ${tag.replace(/^v/i, "")}`;
+    downloadBtn.href = data.html_url;
+  })
+  .catch(() => {
+    downloadBtn.textContent = "Download Latest Release";
+  });
+
+window.addEventListener(
+  "scroll",
+  () => {
+    const threshold = 250;
+    if (window.scrollY > threshold) {
+      if (!navbar.classList.contains("navbar-fixed")) {
+        navbar.classList.add("navbar-fixed");
+        container.style.marginTop = navbar.offsetHeight + "px";
+      }
+    } else {
+      if (navbar.classList.contains("navbar-fixed")) {
+        navbar.classList.remove("navbar-fixed");
+        container.style.marginTop = "";
+      }
+    }
+  },
+  { passive: true },
+);
